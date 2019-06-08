a68ef94c44da9d97c91705c61a150cc07dae5c63
Johnonyango <j.yayah7@gmail.com>
2019-06-08 14:54:23 +0300

    made several modifications to various files

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.col-4').click(function(){
+    $("#p2,#p1,#p3,#p1,#p2,#p3").slideToggle();
+    $("#img1,#img2,#img3").slideToggletoggle();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $('.col-4').click(function(){
+    $("#p2,#p1,#p3,#p1,#p2,p3").fadeToggle();
+    $("#img1,#img2,#img3").fadeToggletoggle();
+  });
+});
