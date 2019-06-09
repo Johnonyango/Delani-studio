4fafc489a3354686b5ce895f7b7a4ac8301a72c1
Johnonyango <j.yayah7@gmail.com>
2019-06-09 21:47:35 +0300

    made modification to several files, e.g added toggle effect in the jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,6 @@
 $(document).ready(function(){
-  $('.col-4').click(function(){
-    $("#p2,#p1,#p3,#p1,#p2,#p3").slideToggle();
-    $("#img1,#img2,#img3").slideToggletoggle();
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
-  $('.col-4').click(function(){
-    $("#p2,#p1,#p3,#p1,#p2,p3").fadeToggle();
-    $("#img1,#img2,#img3").fadeToggletoggle();
+  $('#1').click(function(){
+    $("#p1").fadeToggle();
+    $("#img1").fadeToggle();
   });
 });
